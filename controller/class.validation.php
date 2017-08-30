e93b9f668a7bc76107892187bd0cd15ade32e2b3
Kurvin Hendricks <Kurvin Hendricks>
2017-08-30 21:04:19 +0200

    Adding comments to code

--- controller/class.validation.php
+++ controller/class.validation.php
@@ -1,7 +1,11 @@
 <?php 
 
 include_once(DIRNAME(__FILE__) . '../../traits/trait.numberValidator.php');
-
+/*
+Validation class, 
+uses traits to validate specific inputs
+Author: Kurvin
+*/
 class validation {
 	use numberValidator;	
 }
--- traits/trait.numberValidator.php
+++ traits/trait.numberValidator.php
@@ -1,12 +1,38 @@
 <?php
 
+/*
+Number Validation trait, 
+functions for validating numbers
+Author: Kurvin
+*/
+
 trait numberValidator {
+
+	/*
+	Returns true/false
+	Accepts single argument,
+	checks if argument is fibonacci
+	*/
 	public function isFibonacci($number) {
 		return $this->isSquare( intval( (5*$number*$number) + 4) ) && $this->isSquare( intval( (5*$number*$number) - 4) );
 	}
 
+	/*
+	Returns true/false
+	Accepts single argument
+	checks if argument is square
+	*/
 	public function isSquare($squarableNumber) {
 		$squarootValue = sqrt($squarableNumber);
 		return (($squarootValue*$squarootValue) == $squarableNumber);
+	}
+
+	/*
+	Returns true/false
+	Accepts single argument
+	checks if argument is negative
+	*/
+	public function isNegativeNumber($possibleNegaviteNumber) {
+		return ($possibleNegaviteNumber < 0);
 	}
 }
